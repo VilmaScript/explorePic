06b4549391006a5765cfc2fc2fa74bb064f1c2b1
abara vivian <vilmalamberts10@gmail.com>
2023-10-01 15:11:14 +0100

    added some changes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const apiKey = 'G3eVnhwuDrFoNRrSIs6RCS1MjNpv29rmGiG7eQamvsI'
 
 // Get references to HTML elements
@@ -7,7 +6,7 @@ const searchButton = document.querySelector('.input-icon');
 const imageContainer = document.querySelector('.image-container');
 const ulElement = document.querySelector('.image-container');
 const loader = document.querySelector('.loader');
-
+const errorMessage = document.querySelector('.error-message');
 
 // Unsplash API endpoint with search keyword and access key
 const unsplashApiUrl = `https://api.unsplash.com/search/photos?query=YOUR_SEARCH_KEYWORD&client_id=${apiKey}`;
@@ -16,64 +15,75 @@ const unsplashApiUrl = `https://api.unsplash.com/search/photos?query=YOUR_SEARCH
 const fetchImages = async () => {
  // Clear the previous images
  imageContainer.innerHTML = '';
-    const query = searchInput.value.trim();
-    
-    if (query === '') {
-        alert('Please enter a search term.');
-        return;
-    }
-    // Show the loader while fetching
-  loader.style.display = 'block';
+ errorMessage.style.display = 'none';
+ const query = searchInput.value.trim();
 
-    try {
-        // Update the API URL with the user's query
-        const apiUrl = unsplashApiUrl.replace('YOUR_SEARCH_KEYWORD', query);
-        
-        // Use Fetch API to make the GET request
-        const response = await fetch(apiUrl);
+ if (query === '') {
+  alert('Please enter a search term.');
+  return;
+ }
+ // Show the loader while fetching
+ loader.style.display = 'block';
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
+ try {
+  // Update the API URL with the user's query
+  const apiUrl = unsplashApiUrl.replace('YOUR_SEARCH_KEYWORD', query);
 
-        const images = await response.json();
+  // Use Fetch API to make the GET request
+  const response = await fetch(apiUrl);
 
-        setTimeout(() => {
-         displayImages(images.results);
-         // Hide the loader when rendering is complete
-         loader.style.display = 'none';
-       }, 4000); 
-        
-    } catch (error) {
-        console.error('Error fetching images:', error);
-    } finally {
-    // Hide the loader when done fetching
-    loader.style.display = 'none';
+  if (!response.ok) {
+   throw new Error('Network response was not ok');
   }
+
+  const images = await response.json();
+  // Check if no images were found
+  if (images.results.length === 0) {
+   // Display an error message for no results
+   errorMessage.textContent = 'No images found for the given search term.';
+   errorMessage.style.display = 'block';
+  } else {
+   // Display the images if they are found
+   setTimeout(() => {
+    displayImages(images.results);
+    // Hide the loader when rendering is complete
+    loader.style.display = 'none';
+   }, 4000);
+  }
+ } catch (error) {
+  // Display an error message
+  errorMessage.textContent = 'Error fetching images: ' + error.message;
+  errorMessage.style.display = 'block';
+  loader.style.display = 'none';
+  console.error(error)
+ } finally {
+  // Hide the loader when done fetching
+  loader.style.display = 'none';
+ }
 };
 
 // Function to display fetched images
 const displayImages = (images) => {
 
-    images.forEach((image) => {
-        // Create an <li> element for each image
-        const liElement = document.createElement('li');
-        liElement.classList.add('image-item'); // Optional: Add a CSS class to style each item
+ images.forEach((image) => {
+  // Create an <li> element for each image
+  const liElement = document.createElement('li');
+  liElement.classList.add('image-item'); // Optional: Add a CSS class to style each item
 
-        // Create an <img> element for the image
-        const imgElement = document.createElement('img');
-        imgElement.src = image.urls.small;
-        imgElement.alt = image.alt_description;
+  // Create an <img> element for the image
+  const imgElement = document.createElement('img');
+  imgElement.src = image.urls.small;
+  imgElement.alt = image.alt_description;
 
-        // Append the <img> element to the <li> element
-        liElement.appendChild(imgElement);
+  // Append the <img> element to the <li> element
+  liElement.appendChild(imgElement);
 
-        // Append the <li> element to the <ul> element
-        ulElement.appendChild(liElement);
-    });
+  // Append the <li> element to the <ul> element
+  ulElement.appendChild(liElement);
+ });
 
-    // Append the <ul> element to the image container
-    imageContainer.appendChild(ulElement);
+ // Append the <ul> element to the image container
+ imageContainer.appendChild(ulElement);
 };
 
 // Event listener for the search button
@@ -81,7 +91,7 @@ searchButton.addEventListener('click', fetchImages);
 
 // Event listener for pressing Enter in the input field
 searchInput.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        fetchImages();
-    }
+ if (e.key === 'Enter') {
+  fetchImages();
+ }
 });
